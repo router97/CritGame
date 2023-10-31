05fa23fa1ea90a951881494e25b39943a1f2a3b4
router <you@example.com>
2023-10-31 10:32:36 +0200

    updated shop system (not done)

--- main/main.py
+++ main/main.py
@@ -40,12 +40,29 @@ player_max_health = 100
 player_max_health_base = 100
 player_mana = 100
 player_max_mana = 100
+player_max_mana_base = 100
 player_damage = 99
 player_damage_base = 99
 crit_chance = 4
 enemy_counter = 1
-player_money = 9999
+player_money = 100
 player_input = ''
+
+player_stats = (
+    
+    player_health,
+    player_max_health,
+    player_max_health_base,
+    
+    player_mana,
+    player_max_mana,
+    player_max_mana_base,
+    
+    player_damage,
+    player_damage_base,
+    crit_chance,
+    
+    player_money)
 
 # Functions to handle attacks
 def attack(enemy_health, player_damage):
@@ -60,6 +77,15 @@ def counter_attack(player_health, enemy_damage):
     """Resolves an enemy's counter-attack, reducing the player's health by the enemy's damage."""
     return player_health - enemy_damage
 
+# Healing
+def heal(player_stat, player_max_stat, amount):
+    if player_stat == player_max_stat:
+        pass
+    elif player_stat >= player_max_stat-amount:
+        player_stat = player_max_stat
+    else:
+        player_stat += amount
+    return player_stat, player_max_stat
 
 # Shop functions
 def store_call():
@@ -69,39 +95,57 @@ def store_call():
             print(f"\t{key}: {shop[item_type][key]['name']} ({shop[item_type][key]['price']})")
 
 def buy(player_max_health, player_damage, crit_chance, player_health, player_money, player_max_health_base, player_damage_base):
-    item = player_input.strip("buy").strip().lower()
-    for item_type in shop.keys():
-        for key in shop[item_type].keys():
-            item_key = shop[item_type][key]
-            if item_key["name"] == item:
+    no_money = False
+    item = player_input.strip("buy").strip().lower() # Getting the item name
+    
+    for item_type in shop.keys(): # Searching through item types
+        if no_money: # If the player doesn't have enough money for the item
+            break # Break out of the types loop
+        print('TYPEEEEEE')
+        for key in shop[item_type].keys(): # Searching through items
+            print('item')
+            item_key = shop[item_type][key] # Lessen the adress
+                
+            if item_key["name"] == item: # If the item name matches the input
+                if player_money < item_key["price"]: # If player doesn't have enough money
+                    no_money = True # Setting no_money to True
+                    break # Breaking out of items loop
+                player_money -= item_key['price'] # Reducing player money by item price
+                
+                print(" * bought", item)
                 if item_type == "equipment":
+                    
                     if player_money >= item_key["price"]:
-                        print(" * bought", item)
+                        
                         player_max_health_get = item_key.get("health")
                         player_damage_get = item_key.get("damage")
                         crit_chance_get = item_key.get("crit")
-                        
                         player_max_health = player_max_health_base
                         player_damage = player_damage_base
                         
                         if player_max_health_get is not None:
                             player_max_health += item_key['health']
                             player_health = player_max_health
+                        
                         if player_damage_get is not None:
                             player_damage += item_key['damage']
+                        
                         if crit_chance_get is not None:
                             crit_chance = item_key['crit']
-    #             # if item_type == "consumables":
-    #             #     if item == "health potion":
-    #             #         print(" * bought", item)
-    #             #         Heal(False, None, shop[item_type][key]["heal"])
-    #             #     if item == "mana potion":
-    #             #         print(" * bought", item)
-    #             #         Heal(True, shop[itemType][key]["mana"])
-    #         if player_money >= shop[item_type][key]["price"]:
-    #             player_money -= shop[item_type][key]["price"]
-    return player_max_health, player_damage, crit_chance, player_health, player_money
-
+                
+                elif item_type == "consumables":
+                    if item_key['type'] == 'health':
+                        stats = (player_health, player_max_health)
+                    if item_key['type'] == 'mana':
+                        stats = (player_mana, player_max_mana)
+                    player_stat, player_max_stat = stats
+                    stats = heal(player_stat, player_max_stat, item_key['heal'])  
+            
+                else:
+                    continue 
+    updated_stats = (player_health, player_max_health, player_mana, player_max_mana,
+                     player_damage, crit_chance, player_money)
+    return updated_stats
 
 # Functions to handle round progression
 def win(enemy_counter, enemy_health, enemy_damage, player_money, enemies):
@@ -142,7 +186,7 @@ while playing:
     
     elif player_input[0:3] == 'buy':
         new_values = (buy(player_max_health, player_damage, crit_chance, player_health, player_money, player_max_health_base, player_damage_base))
-        player_max_health, player_damage, crit_chance, player_health, player_money = new_values
+        player_health, player_max_health, player_mana, player_max_mana, player_damage, crit_chance, player_money = new_values
     
     # Counter attack
     if random_int >= 8 and player_input is not 's':
@@ -155,6 +199,6 @@ while playing:
         playing = False
     elif enemy_health <= 0:
         print(" --- You win! ---")
-        enemy_counter, enemy_health, enemy_damage, player_money, enemies = win(enemy_counter, enemy_health, enemy_damage, player_money, enemies)
+        enemy_counter, enemy_health, enemy_damage, player_money = win(enemy_counter, enemy_health, enemy_damage, player_money, enemies)
         if enemy_counter > len(enemies):
             playing = False
